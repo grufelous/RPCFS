43f6bf9cddc96dc074cec88b3a8d356c5c2a82a3
Pratik Anand <grufelous@gmail.com>
2020-11-03 23:20:47 +0530

    Basic server/client arch

--- client.py
+++ client.py
@@ -4,6 +4,7 @@ proxy = ServerProxy('http://localhost:3000')
 
 def cli():
     print('client> ')
+    proxy.do_stuff(10)
 
 if __name__ == '__main__':
     cli()
--- fs.py
+++ fs.py
@@ -1,9 +1,31 @@
 from xmlrpc.client import ServerProxy
+from xmlrpc.server import SimpleXMLRPCServer
 
-proxy = ServerProxy('http://localhost:3000')
+import os
+
+server = SimpleXMLRPCServer(('localhost', 3000), logRequests=True)
+
+# proxy = ServerProxy('http://localhost:3000')
+
+
+def list_directory(dir):
+    return os.listdir(dir)
+
+def do_stuff(arg):
+    return arg*5
+    
+server.register_function(list_directory)
+server.register_function(do_stuff)
 
 def cli():
     print('fs> ')
 
 if __name__ == '__main__':
-    cli()
+    try:
+        print("File server started at port ")
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print("File server closed")
+    
+    
+
