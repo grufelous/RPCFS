83e703427413165c0fc19f23efea52ff6e6a4a33
Pratik Anand <grufelous@gmail.com>
2020-11-05 00:10:35 +0530

    Added basic functions, supported commands, and a tokenizer

--- client.py
+++ client.py
@@ -2,12 +2,42 @@ from xmlrpc.client import ServerProxy
 
 proxy = ServerProxy('http://localhost:7000')
 
+supported_commands = {
+    'pwd': 0,
+    'ls': 1,
+    'cp': 2,
+    'cat': 1,
+    'help': 0,
+    'exit': 0,
+}
 
+def help_message():
+    print('Supported commands: ')
+    print('\tpwd')
+    print('\tls <folder_name>')
+    print('\tcp <source_file> <destination_file>')
+    print('\tcat <file>')
+    print('Use full paths as needed')
 
 def cli():
-    print('client> ')
-    # print(proxy.do_stuff(10))
-    print(proxy.present_working_directory('lol'))
+    str_inp = input('client> ')
+    tokens = ' '.join(str_inp.split(' ')).split()
+    # print(tokens)
+    if len(tokens) == 0:
+        print('Empty input')
+    else:
+        cmd = tokens[0]
+        if cmd not in supported_commands:
+            print('Command not found')
+        elif supported_commands[cmd] != len(tokens)-1:
+            print('Syntax error: argument mismatch')
+        else:
+            print('Success')
+    # print(proxy.present_working_directory('lol'))
 
 if __name__ == '__main__':
-    cli()
+    try:
+        while(True):
+            cli()
+    except KeyboardInterrupt:
+        print('Client terminated')
--- fs.py
+++ fs.py
@@ -3,38 +3,36 @@ from xmlrpc.server import SimpleXMLRPCServer
 
 import os
 
-server = SimpleXMLRPCServer(('localhost', 7000), logRequests=True)
+fs_port = 7000
+
+server = SimpleXMLRPCServer(('localhost', fs_port), logRequests=True)
 
 proxy = ServerProxy('http://localhost:3000')
 
 def present_working_directory(dir):
-    print("Real physical path: {} ".format(os.getcwd()))
+    print('Real physical path: {} '.format(os.getcwd()))
     return os.getcwd()
 
 def list_directory(dir):
+    return os.listdir(dir)
+
+def copy_file(src, dest):
     pass
 
-# def copy(src, dest):
-#     pass
-
-# def cat(file_name):
-#     pass
-
-# def list_directory(dir):
-#     return os.listdir(dir)
-
-# def do_stuff(arg):
-#     return arg*5
+def cat(file_name):
+    pass
 
 server.register_function(list_directory)
 server.register_function(present_working_directory)
+server.register_function(copy_file)
+server.register_function(cat)
 
 def cli():
     print('fs> ')
 
 if __name__ == '__main__':
     try:
-        print("File server started at port ")
+        print('File server started at port {}'.format(fs_port))
         server.serve_forever()
     except KeyboardInterrupt:
-        print("File server closed")
+        print('File server terminated')
